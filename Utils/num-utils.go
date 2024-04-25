0a68767c2de4b0b8db1a245e568280d57b57be55
Oulis Nikolaos <n.oulis@oss.gr>
2024-04-25 20:00:24 +0300

    Changes in num converters

--- Utils/num-utils.go
+++ Utils/num-utils.go
@@ -5,28 +5,49 @@ import (
 	"strings"
 )
 
-func StrToInt8(input interface{}) int8 {
-	return int8(stringToNumberInternal(input.(string), 8))
-}
-
-func StrToInt64(input interface{}) int64 {
-	return stringToNumberInternal(input.(string), 64)
-}
-
-func StrToInt32(input interface{}) int32 {
-	return int32(stringToNumberInternal(input.(string), 32))
-}
-
-func StrToInt16(input interface{}) int16 {
-	return int16(stringToNumberInternal(input.(string), 16))
-}
-
-func stringToNumberInternal(input string, bitSize int) int64 {
-	sourceNumVal, error := strconv.ParseInt(strings.Trim(input, " "), 10, bitSize)
-	if error != nil {
-		panic("Δεν ήταν δυνατή η μετατροπή της συμβολοσειράς σε αριθμό για το πεδίο " + input)
+func StrToInt8(input interface{}) (*int8, error) {
+	res, err := stringToNumberInternal(input.(string), 8)
+	if err != nil {
+		return nil, err
 	}
-	return sourceNumVal
+	res1 := int8(*res)
+	return &res1, err
+}
+
+func StrToInt64(input interface{}) (*int64, error) {
+	res, err := stringToNumberInternal(input.(string), 64)
+	if err != nil {
+		return nil, err
+	}
+	res1 := int64(*res)
+	return &res1, err
+}
+
+func StrToInt32(input interface{}) (*int32, error) {
+	res, err := stringToNumberInternal(input.(string), 32)
+	if err != nil {
+		return nil, err
+	}
+	res1 := int32(*res)
+	return &res1, err
+}
+
+func StrToInt16(input interface{}) (*int16, error) {
+	res, err := stringToNumberInternal(input.(string), 16)
+	if err != nil {
+		return nil, err
+	}
+	res1 := int16(*res)
+	return &res1, err
+}
+
+func stringToNumberInternal(input string, bitSize int) (*int64, error) {
+	sourceNumVal, err := strconv.ParseInt(strings.Trim(input, " "), 10, bitSize)
+	if err != nil {
+		//panic("Δεν ήταν δυνατή η μετατροπή της συμβολοσειράς σε αριθμό για το πεδίο " + input)
+		return nil, err
+	}
+	return &sourceNumVal, nil
 }
 
 func stringToFloatInternal(input string, bitSize int) float64 {
